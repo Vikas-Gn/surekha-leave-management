47d05bb10b4f6c363f7bb9d77fcf9b775caa0648
Vikas-Gn <vikasgn2000@gmail.com>
2025-07-08 13:48:44 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3035;
+const port = process.env.PORT || 3605;
 
 // Middleware
 app.use(cors());
